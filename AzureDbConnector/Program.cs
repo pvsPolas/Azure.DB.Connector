235119764cc89a5aff3e2bc918202ad033835c73
pvsPolas <vanstadenp@gmail.com>
2022-04-10 14:31:56 +0200

    Added Function Scripts

--- AzureDbConnector/Program.cs
+++ AzureDbConnector/Program.cs
@@ -11,32 +11,18 @@ namespace AzureDbConnector
     {
         static void Main(string[] args)
         {
-            try
-            {
-                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-                builder.DataSource = "??.database.windows.net";
-                builder.UserID = "xxx";
-                builder.Password = "xxx";
-                builder.InitialCatalog = "xxx";
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "?.database.windows.net";
+            builder.UserID = "?";
+            builder.Password = "?";
+            builder.InitialCatalog = "?";
 
+            try
+            {  
                 using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
                 {
-                    Console.WriteLine("\nQuery data example:");
-                    Console.WriteLine("=========================================\n");
-
-                    String sql = "SELECT name, collation_name FROM sys.databases";
-
-                    using (SqlCommand command = new SqlCommand(sql, connection))
-                    {
-                        connection.Open();
-                        using (SqlDataReader reader = command.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
-                            }
-                        }
-                    }
+                    //SampleQuery(connection);                    
+                    GetData(connection);
                 }
             }
             catch (SqlException e)
@@ -44,6 +30,47 @@ namespace AzureDbConnector
                 Console.WriteLine(e.ToString());
             }
             Console.ReadLine();
+        }
+
+        static void GetData(SqlConnection connection)
+        {
+            Console.WriteLine("\nStore Proc From Azure");
+            Console.WriteLine("=======================\n");
+
+            String sproc = "sp_Users_GetAllUsers";
+
+            using (SqlCommand command = new SqlCommand(sproc, connection))
+            {
+                connection.Open();
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("{0} {1} {2} {3}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3));
+                    }
+                }
+            }
+        }
+
+        static void SampleQuery(SqlConnection connection)
+        {
+            Console.WriteLine("\nQuery data example:");
+            Console.WriteLine("=========================================\n");
+
+            String sql = "SELECT name, collation_name FROM sys.databases";
+
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                    }
+                }
+            }
         }
     }
 }
